2bb446461b440fc534a36301b4884bda2ca658a8
Ana Luisa Pererira Ferro <analuisa.ferro@pmnfgov.nf>
2022-08-16 17:15:08 -0300

    att

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('cursos/', views.cursos, name="cursos"),
+    path('prematricula/', views.prematricula, name="prematricula"),
+    path('alterarCadastro/', views.alterarCad, name="alterarCad"),
+    path('resultado/', views.resultado, name="resultado"),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'cursos/index.html')
+
+def cursos(request):
+    return render(request, 'cursos/cursos.html')
+
+def prematricula(request):
+    return render(request, 'cursos/pre_matricula.html')
+
+def alterarCad(request):
+    return render(request, 'cursos/alterar_cad.html')
+
+def resultado(request):
+    return render(request, 'cursos/resultado.html')
