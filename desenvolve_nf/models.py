0b8e80b2a926b1cf2665180b40ca3bb68b98b3db
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-03-07 01:29:24 -0300

    07.03.23.2

--- desenvolve_nf/models.py
+++ desenvolve_nf/models.py
@@ -5,7 +5,8 @@ class Carousel_Index(models.Model):
     
     nome = models.CharField(max_length=64, verbose_name="Nome para identificação ou para texto alternativo", blank=False, null=False)
     image = models.ImageField(upload_to='carousel_index/', verbose_name="Imagem 987x394", blank=False, null=True)
+    url = models.CharField(max_length=64, verbose_name="Url, caso tenha para redirecionar", blank=True)
     ativa = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return '%s - %s' % (self.nome, self.id)
