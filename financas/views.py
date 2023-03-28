8b04937cf2571a57fc0977cb1dfdc657687f6f89
Vitor <88908365+VitorAssuncaoSantos@users.noreply.github.com>
2023-03-28 20:52:31 -0300

    28.03.23.4.VA
    Construção da página do ITBI no app financas e alterações no HTML do alvará para padronização dos modelos.

--- financas/views.py
+++ financas/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.apps import apps
 
 # Create your views here.
+
+
 def index(request):
     context = {
         'titulo': apps.get_app_config('financas').verbose_name,
     }
     return render(request, 'financas/index.html', context)
+
 
 def alvara(request):
     context = {
@@ -14,20 +17,22 @@ def alvara(request):
     }
     return render(request, 'financas/alvara.html', context)
 
+
 def get_alvara(request, nome):
-    arquivos={
+    arquivos = {
         'requerimento_alvara': 'REQUERIMENTO-DE-ALVARA.pdf',
         'requerimento_baixa': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
         'requerimento_iss': 'REQUERIMENTO-DE-CERTIDAO-DE-ISS-E-ALVARA-1-4-1.pdf',
         'requerimento_revisao': 'REQUERIMENTO-DE-REVISAO-DE-TAXA-DE-ALVARA-1.pdf'
     }
-    context={
+    context = {
         'titulo': apps.get_app_config('financas').verbose_name,
         'url_pdf': arquivos[nome]
     }
     return render(request, 'financas/get_alvara.html', context)
 
 # Certidão
+
 
 def certidao(request):
     context = {
@@ -37,18 +42,19 @@ def certidao(request):
 
 
 def get_certidao(request, nome):
-    arquivos={
-        'autorizacao_predial':'AUTORIZACAO-VISTORIA-PREDIAL-3.pdf',
-        'requerimento_escritura':'REQUERIMENTO-DE-AVERBACAO-DE-ESCRITURA.pdf',
-        'requerimento_certicao':'REQUERIMENTO-DE-CERTIDAO-1.pdf'
+    tipos_arquivos = {
+        'autorizacao_predial': 'AUTORIZACAO-VISTORIA-PREDIAL-3.pdf',
+        'requerimento_escritura': 'REQUERIMENTO-DE-AVERBACAO-DE-ESCRITURA.pdf',
+        'requerimento_certicao': 'REQUERIMENTO-DE-CERTIDAO-1.pdf'
     }
-    context={
+    context = {
         'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf':arquivos[nome]
+        'url_pdf': tipos_arquivos[nome]
     }
-    return render(request, 'financas/get_certidao.html',context)
+    return render(request, 'financas/get_certidao.html', context)
 
-#IPTU
+# IPTU
+
 
 def iptu(request):
     context = {
@@ -56,20 +62,45 @@ def iptu(request):
     }
     return render(request, 'financas/iptu.html', context)
 
+
 def get_iptu(request, nome):
     arquivos = {
-        'requerimento_baixaDeb':'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
-        'requerimento_inclusao':'REQUERIMENTO-DE-INCLUSAO-DE-POSSUIDOR.pdf',
-        'requerimento_recolhimento':'REQUERIMENTO-DE-RECOLHIMENTO-DE-TRIBUTOS-1.pdf',
-        'requerimento_reviptu':'REQUERIMENTO-REVISAO-DE-IPTU-1.pdf'
+        'requerimento_baixaDeb': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
+        'requerimento_inclusao': 'REQUERIMENTO-DE-INCLUSAO-DE-POSSUIDOR.pdf',
+        'requerimento_recolhimento': 'REQUERIMENTO-DE-RECOLHIMENTO-DE-TRIBUTOS-1.pdf',
+        'requerimento_reviptu': 'REQUERIMENTO-REVISAO-DE-IPTU-1.pdf'
     }
     context = {
         'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf':arquivos[nome]
+        'url_pdf': arquivos[nome]
     }
     return render(request, 'financas/get_iptu.html', context)
 
-#Diversos
+# ITBI
+
+
+def itbi(request):
+    print(request)
+    context = {
+        'titulo': apps.get_app_config('financas').verbose_name,
+    }
+    return render(request, 'financas/itbi.html', context)
+
+
+def get_itbi(request, nome):
+    tipos_arquivos = {
+        'declaracao_ciencia_itbi': 'DECLARACAO-DE-CIENCIA-DE-ITBI.pdf',
+        'declaracao_transacao_imobiliaria': 'Declaracao-de-Transacao-Imo-biliaria-ITBI-PMNF-5.pdf',
+        'requerimento_revisao_itbi': 'REQUERIMENTO-DE-REVISAO-DE-ITBI.pdf'
+    }
+    context = {
+        'titulo': apps.get_app_config('financas').verbose_name,
+        'url_pdf': tipos_arquivos[nome]
+    }
+    return render(request, 'financas/get_itbi.html', context)
+
+# Diversos
+
 
 def diversos(request):
     context = {
@@ -77,15 +108,16 @@ def diversos(request):
     }
     return render(request, 'financas/diversos.html', context)
 
+
 def get_diversos(request, nome):
     arquivos = {
-        'declaracao_residencia':'DECLARACAO-DE-RESIDENCIA.pdf',
-        'requerimento_adesao':'REQUERIMENTO-DE-ADESAO.pdf',
-        'requerimento_att_cadastral':'REQUERIMENTO-DE-ATUALIZACAO-CADASTRAL-1.pdf',
-        'requerimento_autonomo':'REQUERIMENTO-PARA-AUTONOMO.pdf',
+        'declaracao_residencia': 'DECLARACAO-DE-RESIDENCIA.pdf',
+        'requerimento_adesao': 'REQUERIMENTO-DE-ADESAO.pdf',
+        'requerimento_att_cadastral': 'REQUERIMENTO-DE-ATUALIZACAO-CADASTRAL-1.pdf',
+        'requerimento_autonomo': 'REQUERIMENTO-PARA-AUTONOMO.pdf',
     }
     context = {
         'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf':arquivos[nome]
+        'url_pdf': arquivos[nome]
     }
     return render(request, 'financas/get_diversos.html', context)
