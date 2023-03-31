83cdcce194050326ece9cd677450e1e98a1f9b2b
Vitor <88908365+VitorAssuncaoSantos@users.noreply.github.com>
2023-03-31 17:22:49 -0300

    31.03.2023.1.VA
    Melhorias do Layout do diretório financas.

--- financas/views.py
+++ financas/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.apps import apps
 
-# Create your views here.
-
 
 def index(request):
     context = {
@@ -11,104 +9,38 @@ def index(request):
     return render(request, 'financas/index.html', context)
 
 
-def alvara(request, arg):
-    if arg == 'opcoes':
+def formularios(request, arg):
+    if arg == 'formularios':
         context = {
             'titulo': apps.get_app_config('financas').verbose_name,
             'arg': arg
         }
-        return render(request, 'financas/alvara.html', context)
+        return render(request, 'financas/formularios.html', context)
     arquivos = {
+        # Alvará
         'requerimento_alvara': 'REQUERIMENTO-DE-ALVARA.pdf',
         'requerimento_baixa': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
         'requerimento_iss': 'REQUERIMENTO-DE-CERTIDAO-DE-ISS-E-ALVARA-1-4-1.pdf',
-        'requerimento_revisao': 'REQUERIMENTO-DE-REVISAO-DE-TAXA-DE-ALVARA-1.pdf'
-    }
-    context = {
-        'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': arquivos[arg],
-        'arg':arg
-    }
-    return render(request, 'financas/alvara.html', context)
-
-# Certidão
-
-
-def certidao(request, arg):
-    if arg == 'opcoes':
-        context = {
-            'titulo': apps.get_app_config('financas').verbose_name,
-            'arg': arg
-        }
-        return render(request, 'financas/certidao.html', context)
-    tipos_arquivos = {
+        'requerimento_revisao': 'REQUERIMENTO-DE-REVISAO-DE-TAXA-DE-ALVARA-1.pdf',
+        # Certidão
         'autorizacao_predial': 'AUTORIZACAO-VISTORIA-PREDIAL-3.pdf',
         'requerimento_escritura': 'REQUERIMENTO-DE-AVERBACAO-DE-ESCRITURA.pdf',
-        'requerimento_certicao': 'REQUERIMENTO-DE-CERTIDAO-1.pdf'
-    }
-    context = {
-        'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': tipos_arquivos[arg],
-        'arg': arg
-    }
-    return render(request, 'financas/certidao.html', context)
-
-# IPTU
-
-
-def iptu(request, arg):
-    if arg == 'opcoes':
-        context = {
-            'titulo': apps.get_app_config('financas').verbose_name,
-            'arg': arg
-        }
-        return render(request, 'financas/iptu.html', context)
-    arquivos = {
+        'requerimento_certicao': 'REQUERIMENTO-DE-CERTIDAO-1.pdf',
+        # IPTU
         'requerimento_baixaDeb': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
         'requerimento_inclusao': 'REQUERIMENTO-DE-INCLUSAO-DE-POSSUIDOR.pdf',
         'requerimento_recolhimento': 'REQUERIMENTO-DE-RECOLHIMENTO-DE-TRIBUTOS-1.pdf',
-        'requerimento_reviptu': 'REQUERIMENTO-REVISAO-DE-IPTU-1.pdf'
-    }
-    context = {
-        'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': arquivos[arg],
-        'arg': arg
-    }
-    return render(request, 'financas/iptu.html', context)
-
-# ITBI
-
-
-def itbi(request, arg):
-    if arg == 'opcoes':
-        context = {
-            'titulo': apps.get_app_config('financas').verbose_name,
-            'arg': arg
-        }
-        return render(request, 'financas/itbi.html', context)
-    tipos_arquivos = {
+        'requerimento_reviptu': 'REQUERIMENTO-REVISAO-DE-IPTU-1.pdf',
+        # ITBI
         'declaracao_ciencia_itbi': 'DECLARACAO-DE-CIENCIA-DE-ITBI.pdf',
         'declaracao_transacao_imobiliaria': 'Declaracao-de-Transacao-Imo-biliaria-ITBI-PMNF-5.pdf',
-        'requerimento_revisao_itbi': 'REQUERIMENTO-DE-REVISAO-DE-ITBI.pdf'
-    }
-    context = {
-        'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': tipos_arquivos[arg],
-        'arg': arg
-    }
-    return render(request, 'financas/itbi.html', context)
-
-# Isenção
-
-
-def isencao(request, arg):
-    if arg == 'opcoes':
-        context = {
-            'titulo': apps.get_app_config('financas').verbose_name,
-            'arg': arg
-        }
-        return render(request, 'financas/isencao.html', context)
-    tipos_arquivos = {
+        'requerimento_revisao_itbi': 'REQUERIMENTO-DE-REVISAO-DE-ITBI.pdf',
+        # Isenção
+        'requerimento_imunidade_tributaria': 'REQUERIMENTO-DE-IMUNIDADE-TRIBUTARIA-form.pdf',
+        'requerimento_isencao': 'REQUERIMENTO-DE-ISENCAO-MEI-TFA-1.pdf',
+        'requerimento_insencao_nao_incidencia_tributaria': 'REQUERIMENTO-DE-ISENCAO-NAO-INCIDENCIA-TRIBUTARIA.pdf',
+        'requerimento_isencao_parcial_tombamento': 'REQUERIMENTO-DE-ISENCAO-PARCIAL-TOMBAMENTO.pdf',
+        # Diversos
         'requerimento_imunidade_tributaria': 'REQUERIMENTO-DE-IMUNIDADE-TRIBUTARIA-form.pdf',
         'requerimento_isencao': 'REQUERIMENTO-DE-ISENCAO-MEI-TFA-1.pdf',
         'requerimento_insencao_nao_incidencia_tributaria': 'REQUERIMENTO-DE-ISENCAO-NAO-INCIDENCIA-TRIBUTARIA.pdf',
@@ -116,31 +48,142 @@ def isencao(request, arg):
     }
     context = {
         'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': tipos_arquivos[arg],
+        'url_pdf': arquivos[arg],
         'arg': arg
     }
-    return render(request, 'financas/isencao.html', context)
+    return render(request, 'financas/formularios.html', context)
+
+
+# def alvara(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/alvara.html', context)
+#     arquivos = {
+#         'requerimento_alvara': 'REQUERIMENTO-DE-ALVARA.pdf',
+#         'requerimento_baixa': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
+#         'requerimento_iss': 'REQUERIMENTO-DE-CERTIDAO-DE-ISS-E-ALVARA-1-4-1.pdf',
+#         'requerimento_revisao': 'REQUERIMENTO-DE-REVISAO-DE-TAXA-DE-ALVARA-1.pdf'
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/alvara.html', context)
+
+# Certidão
+
+
+# def certidao(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/certidao.html', context)
+#     tipos_arquivos = {
+#         'autorizacao_predial': 'AUTORIZACAO-VISTORIA-PREDIAL-3.pdf',
+#         'requerimento_escritura': 'REQUERIMENTO-DE-AVERBACAO-DE-ESCRITURA.pdf',
+#         'requerimento_certicao': 'REQUERIMENTO-DE-CERTIDAO-1.pdf'
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': tipos_arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/certidao.html', context)
+
+# IPTU
+
+
+# def iptu(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/iptu.html', context)
+#     arquivos = {
+#         'requerimento_baixaDeb': 'REQUERIMENTO-DE-BAIXA-DE-DEBITO.pdf',
+#         'requerimento_inclusao': 'REQUERIMENTO-DE-INCLUSAO-DE-POSSUIDOR.pdf',
+#         'requerimento_recolhimento': 'REQUERIMENTO-DE-RECOLHIMENTO-DE-TRIBUTOS-1.pdf',
+#         'requerimento_reviptu': 'REQUERIMENTO-REVISAO-DE-IPTU-1.pdf'
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/iptu.html', context)
+
+# ITBI
+
+
+# def itbi(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/itbi.html', context)
+#     tipos_arquivos = {
+#         'declaracao_ciencia_itbi': 'DECLARACAO-DE-CIENCIA-DE-ITBI.pdf',
+#         'declaracao_transacao_imobiliaria': 'Declaracao-de-Transacao-Imo-biliaria-ITBI-PMNF-5.pdf',
+#         'requerimento_revisao_itbi': 'REQUERIMENTO-DE-REVISAO-DE-ITBI.pdf'
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': tipos_arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/itbi.html', context)
+
+# Isenção
+
+
+# def isencao(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/isencao.html', context)
+#     tipos_arquivos = {
+#         'requerimento_imunidade_tributaria': 'REQUERIMENTO-DE-IMUNIDADE-TRIBUTARIA-form.pdf',
+#         'requerimento_isencao': 'REQUERIMENTO-DE-ISENCAO-MEI-TFA-1.pdf',
+#         'requerimento_insencao_nao_incidencia_tributaria': 'REQUERIMENTO-DE-ISENCAO-NAO-INCIDENCIA-TRIBUTARIA.pdf',
+#         'requerimento_isencao_parcial_tombamento': 'REQUERIMENTO-DE-ISENCAO-PARCIAL-TOMBAMENTO.pdf'
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': tipos_arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/isencao.html', context)
 
 
 # Diversos
 
 
-def diversos(request, arg):
-    if arg == 'opcoes':
-        context = {
-            'titulo': apps.get_app_config('financas').verbose_name,
-            'arg': arg
-        }
-        return render(request, 'financas/diversos.html', context)
-    arquivos = {
-        'declaracao_residencia': 'DECLARACAO-DE-RESIDENCIA.pdf',
-        'requerimento_adesao': 'REQUERIMENTO-DE-ADESAO.pdf',
-        'requerimento_att_cadastral': 'REQUERIMENTO-DE-ATUALIZACAO-CADASTRAL-1.pdf',
-        'requerimento_autonomo': 'REQUERIMENTO-PARA-AUTONOMO.pdf',
-    }
-    context = {
-        'titulo': apps.get_app_config('financas').verbose_name,
-        'url_pdf': arquivos[arg],
-        'arg': arg
-    }
-    return render(request, 'financas/diversos.html', context)
+# def diversos(request, arg):
+#     if arg == 'opcoes':
+#         context = {
+#             'titulo': apps.get_app_config('financas').verbose_name,
+#             'arg': arg
+#         }
+#         return render(request, 'financas/diversos.html', context)
+#     arquivos = {
+#         'declaracao_residencia': 'DECLARACAO-DE-RESIDENCIA.pdf',
+#         'requerimento_adesao': 'REQUERIMENTO-DE-ADESAO.pdf',
+#         'requerimento_att_cadastral': 'REQUERIMENTO-DE-ATUALIZACAO-CADASTRAL-1.pdf',
+#         'requerimento_autonomo': 'REQUERIMENTO-PARA-AUTONOMO.pdf',
+#     }
+#     context = {
+#         'titulo': apps.get_app_config('financas').verbose_name,
+#         'url_pdf': arquivos[arg],
+#         'arg': arg
+#     }
+#     return render(request, 'financas/diversos.html', context)
