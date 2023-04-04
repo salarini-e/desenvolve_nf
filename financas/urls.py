81b0b813c99d11212cfb1fd2ecefe449fb8910b1
Vitor <88908365+VitorAssuncaoSantos@users.noreply.github.com>
2023-04-04 17:04:31 -0300

    04.04.2023.1.VA

--- financas/urls.py
+++ financas/urls.py
@@ -6,10 +6,10 @@ app_name = 'financas'
 urlpatterns = [
     path('', views.index, name='index'),
     path('formularios/<arg>', views.formularios, name='formularios'),
-    #     path('alvara/<arg>', views.alvara, name='alvara'),
-    #     path('certidao/<arg>', views.certidao, name='certidao'),
-    #     path('iptu/<arg>', views.iptu, name='iptu'),
-    #     path('itbi/<arg>', views.itbi, name='itbi'),
-    #     path("isencao/<arg>", views.isencao, name="isencao"),
-    #     path('diversos/<arg>', views.diversos, name='diversos'),
+    path('alvara/<arg>', views.alvara, name='alvara'),
+    path('certidao/<arg>', views.certidao, name='certidao'),
+    path('iptu/<arg>', views.iptu, name='iptu'),
+    path('itbi/<arg>', views.itbi, name='itbi'),
+    path("isencao/<arg>", views.isencao, name="isencao"),
+    path('diversos/<arg>', views.diversos, name='diversos'),
 ]
