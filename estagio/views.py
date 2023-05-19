aeabe4740107b4b85fd09775933e218dcc1a3d33
Verly-G <gabrielverly03@gmail.com>
2023-05-19 16:49:44 -0300

    19.05.2023.2.GV

--- estagio/views.py
+++ estagio/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 from .models import Vagas, Curso, Secretaria, Universidade
 
 def index(request):
-    universidadeCad = Universidade(nome='CEFET')
-    universidadeCad.save()
-
-    secretariaCad = Secretaria(nome='Secretaria de Ciências', local='Prefeitura')
-    secretariaCad.save()
-
-    cursoCad = Curso(nome='Sistemas da Informação', curso_id_universidade=universidadeCad)
-    cursoCad.save()
-
-    vagasCad = Vagas(nome='Desenvolvimento Web', quantidade_de_vagas=5, vagas_id_secretaria=secretariaCad)
-    vagasCad.save()
-    vagasCad.vagas_id_curso.add(cursoCad)
-    vagasCad.save()
-
     context = {
         'titulo':'Sistema de Estágio',
     }
