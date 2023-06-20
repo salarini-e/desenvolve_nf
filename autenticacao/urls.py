fe3ba2e416b03f9fc8db74cbd3d10e1812c9cca6
ViniciusSoldati <visolmattos10@gmail.com>
2023-06-20 17:43:05 -0300

    20.06.2023.1.VS NÃO SUBIR PARA O TESTE
    Inicio do "esqueci senha", erro com a view do passwd_reset

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 
 app_name='autenticacao'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    
+    path('passwd_reset/', views.passwd_reset, name='passwd_reset'),
+
     path('cadastro/', views.cadastro_user, name='cadastrar_usuario'),
     path('cadastro_aluno/', views.cadastro_aluno, name='cadastrar_aluno'),
+
 
 ]
