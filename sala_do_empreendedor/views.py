a7e9016e6098c955c1d6a48b560527398eb853ef
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-07-25 22:26:18 -0300

    25.07.23.4.ED

--- sala_do_empreendedor/views.py
+++ sala_do_empreendedor/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-         'titulo': 'SALA DO EMPREENDEDOR',
+         'titulo': 'Sala do Empreendedor',
         # 'titulo': apps.get_app_config('financas').verbose_name,
     }
     return render(request, 'sala_do_empreendedor/index.html', context)
