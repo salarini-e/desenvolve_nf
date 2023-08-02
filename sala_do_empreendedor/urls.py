95c4a17031ebfb44fde1fa42ccaea85c421d3ddd
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-08-02 16:00:47 -0300

    02.08.23.2.ED

--- sala_do_empreendedor/urls.py
+++ sala_do_empreendedor/urls.py
@@ -5,4 +5,5 @@ app_name='empreendedor'
 urlpatterns = [
     path('', views.index, name='index'),
     path('quero-ser-mei', views.quero_ser_mei, name='quero_ser_mei'),
+    path('ja-sou-mei', views.ja_sou_mei, name='ja_sou_mei'),
 ]
--- sala_do_empreendedor/views.py
+++ sala_do_empreendedor/views.py
@@ -9,4 +9,15 @@ def index(request):
     return render(request, 'sala_do_empreendedor/index.html', context)
 
 def quero_ser_mei(request):
-    return render(request, 'sala_do_empreendedor/quero-ser-mei.html')
+    context = {
+         'titulo': 'Sala do Empreendedor',
+        # 'titulo': apps.get_app_config('financas').verbose_name,
+    }
+    return render(request, 'sala_do_empreendedor/quero-ser-mei.html', context)
+
+def ja_sou_mei(request):
+    context = {
+         'titulo': 'Sala do Empreendedor',
+        # 'titulo': apps.get_app_config('financas').verbose_name,
+    }
+    return render(request, 'sala_do_empreendedor/ja-sou-mei.html', context)
