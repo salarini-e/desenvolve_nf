ff24d2b975130a945d027bd9f21c7a4406833b69
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-08-08 15:37:47 -0300

    08.08.23.1.ED

--- sala_do_empreendedor/urls.py
+++ sala_do_empreendedor/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('consultar-protocolo', views.consultar_protocolo, name='consultar_protocolo'),
     path('faccao-legal', views.faccao_legal, name='faccao_legal'),
     path('vitrine-virtual', views.vitrine_virtual, name='vitrine_virtual'),
+    
     path('quero-ser-mei', views.quero_ser_mei, name='quero_ser_mei'),
+    path('quero-ser-mei/por-que-ser-mei', views.por_que_ser_mei, name='por_que_ser_mei'),
+    path('quero-ser-mei/o-que-voce-precisa-saber-antes-de-se-tornar-um-mei', views.o_que_precisa_saber_para_ser_mei, name='o_que_precisa_saber_para_ser_mei'),
     path('ja-sou-mei', views.ja_sou_mei, name='ja_sou_mei'),
 ]
