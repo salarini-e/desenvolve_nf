e2ac8157dd7a89aae2c1d389f4ca23b7140980b3
Vítor Assunção dos Santos <vitor.assuncaosantos100@gmail.com>
2023-09-14 17:44:48 -0300

    14.09.2023.1.assuncao-v(Aprimorando o menu do 'quero-ser-mei' da aplicação 'sala-do-empreendedor'){DIA DE ESTÁGIO}

--- sala_do_empreendedor/urls.py
+++ sala_do_empreendedor/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('faccao-legal', views.faccao_legal, name='faccao_legal'),
     path('vitrine-virtual', views.vitrine_virtual, name='vitrine_virtual'),
     path('cadastro-fornecedores-e-compras-publicas', views.cadastro_fornecedores_e_compras_publicas, name='compras_publicas'),
+    
     path('quero-ser-mei', views.quero_ser_mei, name='quero_ser_mei'),
     path('quero-ser-mei/por-que-ser-mei', views.por_que_ser_mei, name='por_que_ser_mei'),
     path('quero-ser-mei/o-que-voce-precisa-saber-antes-de-se-tornar-um-mei', views.o_que_precisa_saber_para_ser_mei, name='o_que_precisa_saber_para_ser_mei'),
