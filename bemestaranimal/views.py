4b85ad12e566c775857023d359b3b4f82e8026c9
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-12-21 14:46:35 -0300

    Update views.py

--- bemestaranimal/views.py
+++ bemestaranimal/views.py
@@ -257,11 +257,12 @@ def tornarParceiro(request):
                         parceiro.user_register=request.user
                         parceiro.save()
                         messages.success(request, 'Parabéns! Agora é só esperar a validação de um agente da SSUBEA.')
+                        return redirect('bemestaranimal:descontar_token')
                     else:
                         messages.error(request, 'Você não é o responsável por esta empresa.')
                         return redirect('bemestaranimal:tornarParceiro')
                 
-                return redirect('bemestaranimal:descontar_token')
+                
         else:
             messages.error(request, 'Você precisa estar ciente dos termos de parceria da SSUBEA para se tornar um parceiro.')
     empresas = Empresa.objects.filter(user_register=request.user)
