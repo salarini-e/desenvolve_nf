5a0660ffa7276cc4a70c5b961c9105ae90113640
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-12-21 15:36:18 -0300

    21.12.23.3.ED

--- sala_do_empreendedor/views_folder/vitrine_virtual.py
+++ sala_do_empreendedor/views_folder/vitrine_virtual.py
@@ -49,46 +49,54 @@ def minha_vitrine(request, id):
     
 @login_required
 def enviar_ou_trocar_logo(request, id):
-    empresa=Empresa.objects.get(id=id)
-    if request.user.is_staff or request.user==empresa.user_register:    
-        rg_vitrine=Registro_no_vitrine_virtual.objects.get(empresa=empresa)
-        form=FormLogoEmpresa(instance=rg_vitrine)
-        if request.method == 'POST':
-            form = FormLogoEmpresa(request.POST, request.FILES, instance=rg_vitrine)
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Logo alterada com sucesso!')
-                return redirect('empreendedor:minha_vitrine', id=id)
-        context={
-            'form': form,
-            'titulo': 'Sala do Empreendedor - Minha Empresa - Enviar ou Trocar Logo'            
-            }
-        return render(request, 'sala_do_empreendedor/vitrine-virtual/enviar_ou_trocar_logo.html', context)
-    messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
-    return redirect('empreendedor:minha_empresa')    
+    empresa=Empresa.objects.get(id=id)  
+    if empresa.register_user == request.user:  
+        if request.user.is_staff or request.user==empresa.user_register:    
+            rg_vitrine=Registro_no_vitrine_virtual.objects.get(empresa=empresa)
+            form=FormLogoEmpresa(instance=rg_vitrine)
+            if request.method == 'POST':
+                form = FormLogoEmpresa(request.POST, request.FILES, instance=rg_vitrine)
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, 'Logo alterada com sucesso!')
+                    return redirect('empreendedor:minha_vitrine', id=id)
+            context={
+                'form': form,
+                'titulo': 'Sala do Empreendedor - Minha Empresa - Enviar ou Trocar Logo'            
+                }
+            return render(request, 'sala_do_empreendedor/vitrine-virtual/enviar_ou_trocar_logo.html', context)
+        messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
+        return redirect('empreendedor:minha_empresa')   
+    else:
+        messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
+        return redirect('empreendedor:minha_empresa') 
 
 @login_required()
 def casdastrar_produto(request, id):
-    empresa=Empresa.objects.get(id=id)    
-    if request.user.is_staff or request.user==empresa.user_register:
-        rg_vitrine=Registro_no_vitrine_virtual.objects.get(empresa=empresa)
-        form=FormCadastrarProduto(initial={'rg_vitrine': rg_vitrine.id})
-        if request.method == 'POST':
-            form = FormCadastrarProduto(request.POST, request.FILES)
-            if form.is_valid():
-                produto=form.save(commit=False)
-                produto.rg_vitrine=rg_vitrine
-                produto.save()
-                messages.success(request, 'Produto cadastrado com sucesso!')
-                return redirect('empreendedor:minha_vitrine', id=id)
-        context={
-            'form': form,
-            'titulo': 'Sala do Empreendedor - Vitrine Virtual - Cadastrar produto'
-            }
-        return render(request, 'sala_do_empreendedor/vitrine-virtual/cadastrar_produto.html', context)
-    messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
-    return redirect('empreendedor:minha_empresa')
-
+    empresa=Empresa.objects.get(id=id)  
+    if empresa.register_user == request.user:  
+        if request.user.is_staff or request.user==empresa.user_register:
+            rg_vitrine=Registro_no_vitrine_virtual.objects.get(empresa=empresa)
+            form=FormCadastrarProduto(initial={'rg_vitrine': rg_vitrine.id})
+            if request.method == 'POST':
+                form = FormCadastrarProduto(request.POST, request.FILES)
+                if form.is_valid():
+                    produto=form.save(commit=False)
+                    produto.rg_vitrine=rg_vitrine
+                    produto.save()
+                    messages.success(request, 'Produto cadastrado com sucesso!')
+                    return redirect('empreendedor:minha_vitrine', id=id)
+            context={
+                'form': form,
+                'titulo': 'Sala do Empreendedor - Vitrine Virtual - Cadastrar produto'
+                }
+            return render(request, 'sala_do_empreendedor/vitrine-virtual/cadastrar_produto.html', context)
+        messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
+        return redirect('empreendedor:minha_empresa')
+    else:
+        messages.error(request, 'Você não tem autorização para acessar a página solicitada!')
+        return redirect('empreendedor:minha_empresa')
+    
 @login_required()
 def cadastrar_produto_vitrine(request):
     form=FormEmpresa()
