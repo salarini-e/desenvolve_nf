722ea28b455561c48ede5ec74863a58a991e4005
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2024-01-25 12:26:16 -0300

    Update views.py

--- cursos/views.py
+++ cursos/views.py
@@ -420,7 +420,7 @@ def area_do_estudante(request):
         aluno=Aluno.objects.get(pessoa=pessoa)
     except:
         messages.error(request, 'Você não é um aluno! Primeiro você deve se inscrever em um curso.')
-        return redirect('cursos:cursos')
+        return redirect('cursos:cursos', tipo='cursos')
     matriculas=Matricula.objects.filter(aluno=aluno).order_by('-turma__data_inicio')
     alertas=Alertar_Aluno_Sobre_Nova_Turma.objects.filter(aluno=aluno, alertado=False).order_by('-dt_inclusao')
     context={
