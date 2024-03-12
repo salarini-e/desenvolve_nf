5fd55ff9bfb057701975315a9919868edd52aeb3
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-03-12 13:32:31 -0300

    12.03.24.1.ED

--- cursos_empresariais/custom_empresarial.py
+++ cursos_empresariais/custom_empresarial.py
@@ -1,6 +1,6 @@
 from django import template
-from cursos.models import Alertar_Aluno_Sobre_Nova_Turma
-from cursos.models import Matricula, Turma
+from .models import Alertar_Aluno_Sobre_Nova_Turma
+from .models import Matricula, Turma
 register = template.Library()
 
 @register.filter
