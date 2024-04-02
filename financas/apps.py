3a7c8d327198202bb49dc3197966a34098edc251
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-04-02 15:58:39 -0300

    02.04.24.1-2.ED

--- financas/apps.py
+++ financas/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class FinancasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'financas'
-    verbose_name = 'Finanças'
+    verbose_name = 'Secretaria de Finanças'
